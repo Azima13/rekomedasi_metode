ad168913392abde2e3d067c75b8cb9cca2b0c013
Azima13 <anggagardiawan@gmail.com>
2020-01-31 01:07:55 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 $dbhost='wisata-sql.mysql.database.azure.com';
 $dbuser='mysqldbuser@wisata-sql';
 $dbpass='azima0311#';
-$dbname='wisata-sql';
+$dbname='db_wisata_lombok';
 //-- database connections
 $db=new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 //-- halt and show error message if connection fail
